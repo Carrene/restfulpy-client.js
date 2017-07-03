1043cc031c15020af6bb99021cbb9648ebec709a
mrastiak <armin.ayari@gmail.com>
2017-07-03 18:50:16 +0430

    Created default class

---
+++ index.js
@@ -0,0 +1,90 @@
+
+export default class APIClient {
+  constructor (vm, version, entity) {
+    this.url = `${version}/${entity}`
+    this.vm = vm
+    this.status = ''
+    this.message = ''
+  }
+  request (verb, data, options) {
+    var self = this
+    if (!options) {
+      options = {}
+    }
+    var success = options.success
+    var error = options.error
+
+    jQuery.extend(options, {
+      method: verb,
+      data: data,
+      headers: httpHeaders,
+      dataType: 'json',
+      success (data, textStatus, xhr) {
+        auth.checkResponse(xhr)
+        self.status = xhr.status
+        if (success) {
+          success.bind(self.vm)(data, new ResponseMetadata(xhr))
+        }
+      },
+      error (xhr, textStatus, err) {
+        auth.checkResponse(xhr)
+        self.status = xhr.status
+        if (error) {
+          error.bind(self.vm)(err, xhr)
+        }
+      },
+      xhrFields: {
+        withCredentials: true // FIXME: send only on development env
+      }
+    })
+
+    jQuery.ajax(`${BASE_URL}/${this.url}${!options.path ? '' : options.path}`, options)
+  }
+  get (data, options) {
+    this.request('get', data, options)
+  }
+  post (data, options) {
+    this.request('post', data, options)
+  }
+  put (data, options) {
+    this.request('put', data, options)
+  }
+  delete_ (data, options) {
+    this.request('delete', data, options)
+  }
+  patch (data, options) {
+    this.request('patch', data, options)
+  }
+  undelete (data, options) {
+    this.request('undelete', data, options)
+  }
+  // Promises
+  $request (verb, data, options) {
+    return new Promise((resolve, reject) => this.request(verb, data, jQuery.extend({}, options, {
+      success (resp) {
+        resolve(resp)
+      },
+      error (resp) {
+        reject(resp)
+      }
+    })))
+  }
+  $post (data, options) {
+    return this.$request('post', data, options)
+  }
+  $get (data, options) {
+    return this.$request('get', data, options)
+  }
+  $put (data, options) {
+    return this.$request('put', data, options)
+  }
+  $patch (data, options) {
+    return this.$request('patch', data, options)
+  }
+  $delete (data, options) {
+    return this.$request('delete', data, options)
+  }
+  $undelete (data, options) {
+    return this.$request('undelete', data, options)
+  }
+}

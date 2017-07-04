33d4f17148efb33c0cece3e5bb9cf65f8923b1d6
pylover <vahid.mardani@gmail.com>
2017-07-05 04:08:58 +0430

    addQueryStringParameter (key, value, allowDuplicatedKeys = false)

--- src/request.js
+++ src/request.js
@@ -5,6 +5,7 @@ export default class Request {
   constructor (context, payload) {
     this.context = context
     this.payload = payload || {}
+    this.queryString = []
   }
 
   addAuthenticationHeaders (force = false) {
@@ -18,10 +19,24 @@ export default class Request {
 
   removeAuthenticationHeaders () {
     this.context.authenticator.removeAuthenticationHeaders(this)
+    return this
   }
 
   addParameters (parameters) {
     Object.assign(this.payload, parameters)
+    return this
+  }
+
+  addQueryStringParameter (key, value, allowDuplicatedKeys = false) {
+    if (!allowDuplicatedKeys) {
+      for (let i of this.queryString) {
+        if (key === i[0]) {
+          i[1] = value
+        }
+      }
+    } else {
+      this.queryString.push([key, value])
+    }
   }
 
 }

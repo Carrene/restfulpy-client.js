0c1ab1aad4ed545cf105545d977c1223e3e7d93c
pylover <vahid.mardani@gmail.com>
2017-07-05 23:56:53 +0430

    Adding test framework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # restfulpy-client.js
+
+```bash
+echo "export CHROME_BIN=$(which chromium-browser)" >> ~/.bashrc
+```
---
+++ tests/client.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vahid on 7/5/17.
+ */
+
+import { describe } from 'karma'
+
+describe('Calculator', function() {
+
+	it('should add numbers')
+
+});

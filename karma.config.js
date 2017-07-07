6f7d9fa63ef70c25273d4b228ef164baf07edc47
pylover <vahid.mardani@gmail.com>
2017-07-08 02:44:21 +0430

    STarting and terminating the mockup-server.py before and after each test

--- karma.config.js
+++ karma.config.js
@@ -85,7 +85,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', './mockup.sh'],
+    browsers: ['Chrome', './tests/mockup-server.py'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- tests/mockup-server.py
+++ tests/mockup-server.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 
 from os.path import abspath, dirname
 

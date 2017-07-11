80545dc35c4f5dfb0aa22ef490b3df348db8d961
pylover <vahid.mardani@gmail.com>
2017-07-11 18:03:34 +0430

    Ignoring coverage at all. because all the coverage libraries in nodejs are buggy and not working with babel transpoiled codes. damn!

--- karma.config.js
+++ karma.config.js
@@ -4,28 +4,16 @@ const path = require('path')
 
 module.exports = function (config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
     files: [
       'src/**/*.js',
       'tests/**/*.js'
     ],
-
-    // list of files to exclude
     exclude: ['dist'],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['webpack'],
-      'tests/**/*.js': ['webpack']
+      'src/**/*.js': ['webpack', 'sourcemap'],
+      'tests/**/*.js': ['webpack', 'sourcemap']
     },
     babelPreprocessor: {
       options: {
@@ -48,68 +36,24 @@ module.exports = function (config) {
       // webpack watches dependencies
 
       // webpack configuration
-      target: 'node',
       resolve: {
         alias: {
           'restfulpy': path.resolve(__dirname, 'src')
         }
       }
-      // ,
-      // node: {
-      //   'fs': 'empty'
-      // }
-      // resolve: {
-      //   extensions: ['.js', '.json'],
-      //   modules: [path.resolve(__dirname, 'node_modules')],
-      //   alias: {
-      //     'src': path.resolve(__dirname, 'src')
-      //   }
-      // }
-      // resolveLoader: {
-      //   modules: [path.join(__dirname, 'node_modules')],
-      //   extensions: ['.js', '.json'],
-      //   mainFields: ['loader', 'main']
-      // }
     },
 
     webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
       stats: 'errors-only'
     },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/'
-    },
-
-    // web server port
+    reporters: ['progress'],
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   })
 }

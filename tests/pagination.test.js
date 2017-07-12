cc64a9cf14bd5a4c6377bd209b9009409166fc96
pylover <vahid.mardani@gmail.com>
2017-07-13 01:24:39 +0430

    Pagination test case

--- tests/pagination.test.js
+++ tests/pagination.test.js
@@ -1,19 +1,40 @@
-// /**
-//  * Created by vahid on 7/13/17.
-//  */
-// /**
-//  * Created by vahid on 7/9/17.
-//  */
-//
-// import { MockupClient } from './helpers'
-//
-// describe('Pagination', function () {
-//   it('First page', function (done) {
-//     let c = new MockupClient()
-//     c.request('resources').take('id', 1).done().then((resp) => {
-//       expect(resp.json.length).toEqual(1)
-//       expect(resp.json[0]).toEqual({id: 1, title: 'resource1'})
-//       done()
-//     })
-//   })
-// })
+/**
+ * Created by vahid on 7/13/17.
+ */
+
+import { MockupClient } from './helpers'
+
+describe('Pagination', function () {
+  it('First page', function (done) {
+    let c = new MockupClient()
+    c.request('resources').sort('id').take(3).done().then((resp) => {
+      expect(resp.json.length).toEqual(3)
+      expect(resp.json[0]['id']).toEqual(1)
+      done()
+    })
+  })
+  it('Second page', function (done) {
+    let c = new MockupClient()
+    c.request('resources').sort('id').skip(3).take(3).done().then((resp) => {
+      expect(resp.json.length).toEqual(3)
+      expect(resp.json[0]['id']).toEqual(4)
+      done()
+    })
+  })
+  it('Third page', function (done) {
+    let c = new MockupClient()
+    c.request('resources').sort('id').skip(6).take(3).done().then((resp) => {
+      expect(resp.json.length).toEqual(3)
+      expect(resp.json[0]['id']).toEqual(7)
+      done()
+    })
+  })
+  it('Last page', function (done) {
+    let c = new MockupClient()
+    c.request('resources').sort('id').skip(9).take(3).done().then((resp) => {
+      expect(resp.json.length).toEqual(1)
+      expect(resp.json[0]['id']).toEqual(10)
+      done()
+    })
+  })
+})

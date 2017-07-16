d143c0cdb06dc42bdd4dc18fb649a74850270ccb
mrastiak <armin.ayari@gmail.com>
2017-07-16 17:00:09 +0430

    Filtering sorting pagination test

---
+++ tests/filtering.sorting.pagination.test.js
@@ -0,0 +1,14 @@
+
+import { MockupClient } from './helpers'
+
+describe('Filtering Sorting Pagination', function () {
+  it('LIKE filter Descending Second page', function (done) {
+    let c = new MockupClient()
+    c.request('resources').filter('title', '%resource').sort('-id').skip(3).take(3).done().then((resp) => {
+      expect(resp.json.length).toEqual(3)
+      expect(resp.json[0]['id']).toEqual(7)
+      expect(resp.json[2]['id']).toEqual(5)
+      done()
+    })
+  })
+})

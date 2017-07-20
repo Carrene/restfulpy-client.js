eaf9e5097e5d551362fb1687f49e1a235c4187c4
pylover <vahid.mardani@gmail.com>
2017-07-21 02:16:58 +0430

    Implementing a state machine to handle the model states

--- src/helpers.js
+++ src/helpers.js
@@ -9,3 +9,22 @@ export function encodeQueryString (obj) {
     return Object.keys(obj).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&')
   }
 }
+
+// eslint-disable-next-line no-extend-native
+String.prototype.getHashCode = function () {
+  let hash = 0
+  if (this.length === 0) {
+    return hash
+  }
+  for (let i = 0; i < this.length; i++) {
+    hash = ((hash << 5) - hash) + this.charCodeAt(i)
+    hash = hash & hash // Convert to 32bit integer
+  }
+  return hash
+}
+
+export function getObjectHashCode (obj) {
+  let items = Object.keys(obj).map(k => [k, obj[k].toString()])
+  items.sort()
+  return encodeQueryString(items).getHashCode()
+}
--- src/model.js
+++ src/model.js
@@ -2,6 +2,9 @@
  * Created by vahid on 7/15/17.
  */
 import Field from './field'
+import { getObjectHashCode } from './helpers'
+
+class ModelStateError extends Error {}
 
 const instanceHandler = {
   get (target, name) {
@@ -16,13 +19,65 @@ const instanceHandler = {
 
   set (target, name, value) {
     if (name in target.constructor.fields) {
+      if (target.__status__ === 'deleted') {
+        throw new ModelStateError('Cannot change deleted object.')
+      }
       if (target.data[name] !== value) {
         target.data[name] = value
-        target.dirty = true
+        target.changed()
       }
       return
     }
     target[name] = value
+  }
+}
+
+const modelPrototype = {
+  get __identity__ () {
+    return this.constructor.__primaryKeys__.map(k => this[k])
+  },
+  get resourcePath () {
+    return `${this.__url__}/${this.__identity__.join('/')}`
+  },
+  changed () {
+    this.__hash__ = this.constructor.getHashCode(this.data)
+    if (this.__status__ === 'loaded') {
+      this.__status__ = (this.__server_hash__ === this.__hash__) ? 'loaded' : 'dirty'
+    }
+  },
+  update (obj) {
+    Object.assign(this.data, obj)
+  },
+  delete () {
+    switch (this.__status__) {
+      case 'new':
+        throw new ModelStateError('Cannot delete unsaved objects.')
+      case 'deleted':
+        throw new ModelStateError('Object is already deleted.')
+      case 'dirty':
+        throw new ModelStateError('Object is dirty and cannot delete a dirty object, use reload to reset it\'s state')
+    }
+    return new Promise((resolve, reject) => {
+      this.constructor.__client__.delete(this.resourcePath).done().then(resp => {
+        this.update(resp.json)
+        this.__status__ = 'deleted'
+        resolve(this)
+      }, resp => {
+        // Error
+        reject(resp)
+      })
+    })
+  },
+  reload () {
+
+  },
+  save () {
+    switch (this.__status__) {
+      case 'loaded':
+        throw new ModelStateError('Object is not changed.')
+      case 'deleted':
+        throw new ModelStateError('Object is deleted.')
+    }
   }
 }
 
@@ -34,28 +89,34 @@ const DEFAULT_VERBS = {
 }
 
 export default function createModelClass (name, options, client, metadata) {
-  let Model = function (data) {
-    this.dirty = false
+  let Model = function (data, status = 'new') {
+    this.__status__ = status
+    this.__hash__ = 0
+    this.__server_hash__ = (status === 'loaded') ? this.constructor.getHashCode(data) : 0
     this.data = data
+    this.constructor = Model
+    this.changed()
     return new Proxy(this, instanceHandler)
   }
+  Model.prototype = modelPrototype
+
   // Defining a name for our class
   Reflect.defineProperty(Model, 'name', {value: name})
-  // URL
-  Model.__url__ = options.url
+  // Storing some static variables
   // Creating a static member consist of of all fields
   Model.fields = {}
   for (let k in metadata.fields) {
     Model.fields[k] = new Field(metadata.fields[k])
   }
-  // Storing some static variables
   Model.__client__ = client
-  Model.primaryKeys = metadata.primaryKeys
+  Model.__url__ = options.url
+  Model.__primaryKeys__ = metadata.primaryKeys
   // Creating HTTP shorthands
-  let verbs = Object.assign({}, DEFAULT_VERBS, options.verbs || {})
+  Model.__verbs__ = Object.assign({}, DEFAULT_VERBS, options.verbs || {})
+  Model.getHashCode = getObjectHashCode
   Model.load = (...filters) => {
     return new Promise((resolve, reject) => {
-      Model.__client__.request(Model.__url__, verbs.load).filter(...filters).done().then(resp => {
+      Model.__client__.request(Model.__url__, Model.__verbs__.load).filter(...filters).done().then(resp => {
         resolve(resp.json.map(o => new Model(o)))
       }, resp => {
         // Error
@@ -63,8 +124,10 @@ export default function createModelClass (name, options, client, metadata) {
       })
     })
   }
-  // for (let verb in verbs) {
+  // for (let method in verbs) {
+  //   Model[method] = (...queryString) => {
   //
+  //   }
   // }
   return Model
 }

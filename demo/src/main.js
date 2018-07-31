da9eda31f188bcd11a09b8fa0b9ebb1dda4f7e01
shirin <eghrarish@gmail.com>
2018-07-31 16:21:46 +0430

    Implemented resource form with model.js, #37

--- demo/src/main.js
+++ demo/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router.js'
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 require('./styles/index.sass')
 
--- demo/src/pages/Resource.vue
+++ demo/src/pages/Resource.vue
@@ -1,10 +1,57 @@
 <template>
   <div class="resource">
+
+    <h3>Form</h3>
+
     <form @submit.prevent="update" class="form">
-      <input type="text" class="input" placeholder="Title" v-model="resource.title">
-      <button type="submit" class="button"> Update </button>
+      <label for="title">{{ Resource.fields.title.label }}</label>
+      <input
+        id="title"
+        type="text"
+        class="input"
+        :placeholder="Resource.fields.title.watermark"
+        v-model="$v.resource.title.$model"
+        :class="$v.resource.title.$error ? 'error' : null"
+      >
+      <button
+        v-if="resource.__status__ === 'new'"
+        :disabled="$v.resource.title.$invalid"
+        type="submit"
+        class="button"
+      >
+        Save
+      </button>
+      <button
+        v-else
+        type="submit"
+        :disabled="$v.resource.title.$invalid || resource.__status__ === 'loaded'"
+        class="button"
+      >
+        Update
+      </button>
+      <button type="button" @click="reload">Reload</button>
+      <button type="button" @click="deleteResource">Delete</button>
     </form>
+
+    <hr>
+
+    <h3>Selected Resource</h3>
+
     {{ resource }}
+
+    <hr>
+
+    <h3>Resources</h3>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+      </tr>
+      <tr v-for="singleResource in resources" :key="singleResource.id" @click="resource = singleResource">
+        <td>{{ singleResource.id }}</td>
+        <td>{{ singleResource.title }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -14,19 +61,47 @@ import server from './../server'
 export default {
   data () {
     return {
-      resource: new server.metadata.models.Resource()
+      resource: new server.metadata.models.Resource(),
+      Resource: server.metadata.models.Resource,
+      resources: []
     }
   },
-  methods: {
-    update () {
-      console.log('Update')
+  validations () {
+    return {
+      resource: {
+        title: server.metadata.models.Resource.fields.title.createValidator()
+      }
     }
   },
   mounted () {
-    console.log(this.resource)
-    server.metadata.models.Resource.get('1').then(resp => {
-      console.log(resp)
-    })
+    this.updateResources()
+  },
+  methods: {
+    update () {
+      this.resource.save().send().then(resp => {
+        if (resp.status === 200) {
+          this.resource = resp.models
+          this.updateResources()
+        }
+      })
+    },
+    updateResources () {
+      server.metadata.models.Resource.load().send().then(resp => {
+        this.resources = resp.models
+      })
+    },
+    reload () {
+      this.resource.reload().send().then(resp => {
+        this.resource = resp.models
+      })
+    },
+    deleteResource () {
+      this.resource.delete().send().then(resp => {
+        if (resp.status === 200) {
+          this.updateResources()
+        }
+      })
+    }
   }
 }
 </script>

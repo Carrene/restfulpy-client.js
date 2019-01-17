11a107e7e7433e61c619b5845a9415bdfa656d6f
Mahdi Aryayi <mahdiaryayi@gmail.com>
2019-01-17 14:07:22 +0330

    Added coverage for pagination, #80

--- tests/exceptions.test.js
+++ tests/exceptions.test.js
@@ -3,7 +3,7 @@ import {
   AbstractBaseClassError,
   MethodMustOverrideError
 } from '../src/exceptions'
-import { MockupClient, FakeMockupClient } from './helpers'
+import { FakeMockupClient } from './helpers'
 
 describe('Abstract Authenticator Error', function () {
   it('instantiation', function (done) {
@@ -15,18 +15,17 @@ describe('Abstract Authenticator Error', function () {
 })
 
 describe('Abstract login and logout', function () {
-  let fakeClient = new FakeMockupClient()
-  let client = new MockupClient()
-  client.logout()
-  it('Login', function (done) {
-    expect(() =>
-      fakeClient.login({ email: 'user1@example.com', password: '123456' })
-    ).toThrow(new MethodMustOverrideError())
+  let c = new FakeMockupClient()
+  it('Logout', function (done) {
+    expect(() => c.logout()).toThrow(new MethodMustOverrideError())
     done()
   })
 
-  it('Logout', function (done) {
-    expect(() => fakeClient.logout()).toThrow(new MethodMustOverrideError())
+  it('Login', function (done) {
+    expect(() => {
+      window.localStorage.removeItem('token')
+      return c.login({ email: 'user1@example.com', password: '123456' })
+    }).toThrow(new MethodMustOverrideError())
     done()
   })
 })

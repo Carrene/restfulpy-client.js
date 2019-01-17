cdbeefbdc1e586be4f63ad507ac04d308d6a12a1
Mahdi Aryayi <mahdiaryayi@gmail.com>
2019-01-17 13:30:27 +0330

    Removed identity and authenticated from response, #80

--- tests/exceptions.test.js
+++ tests/exceptions.test.js
@@ -3,7 +3,7 @@ import {
   AbstractBaseClassError,
   MethodMustOverrideError
 } from '../src/exceptions'
-import { FakeMockupClient } from './helpers'
+import { MockupClient, FakeMockupClient } from './helpers'
 
 describe('Abstract Authenticator Error', function () {
   it('instantiation', function (done) {
@@ -15,17 +15,18 @@ describe('Abstract Authenticator Error', function () {
 })
 
 describe('Abstract login and logout', function () {
-  let c = new FakeMockupClient()
-
+  let fakeClient = new FakeMockupClient()
+  let client = new MockupClient()
+  client.logout()
   it('Login', function (done) {
     expect(() =>
-      c.login({ email: 'user1@example.com', password: '123456' })
+      fakeClient.login({ email: 'user1@example.com', password: '123456' })
     ).toThrow(new MethodMustOverrideError())
     done()
   })
 
   it('Logout', function (done) {
-    expect(() => c.logout()).toThrow(new MethodMustOverrideError())
+    expect(() => fakeClient.logout()).toThrow(new MethodMustOverrideError())
     done()
   })
 })
